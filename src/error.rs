d060eb484d8e7c348da78da1ae8e1028941f5709
Jayce Fayne <jayce.fayne@mailbox.org>
2026-04-26 10:46:36 +0200

    remove `Debug` bound from `T`

--- src/error.rs
+++ src/error.rs
@@ -1,23 +1,29 @@
-use core::fmt;
+use core::error::Error;
+use core::fmt::{Debug, Display, Formatter, Result};
 
-#[derive(Debug)]
 pub struct SendError<T>(pub T);
 
-impl<T> fmt::Display for SendError<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl<T> Debug for SendError<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "channel closed")
     }
 }
 
-impl<T: fmt::Debug> core::error::Error for SendError<T> {}
+impl<T> Display for SendError<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "channel closed")
+    }
+}
+
+impl<T> Error for SendError<T> {}
 
 #[derive(Debug)]
 pub struct RecvError;
 
-impl fmt::Display for RecvError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl Display for RecvError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "channel closed")
     }
 }
 
-impl core::error::Error for RecvError {}
+impl Error for RecvError {}
